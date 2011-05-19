fa6a17bfd53b09ba29637e806baf247c346424d1
xy124 <qcd@gmx.net>
2011-05-19 17:47:38 +0200

    improved cloudmechanism

--- source/AttackAnimations/CAA_Cloud.cpp
+++ source/AttackAnimations/CAA_Cloud.cpp
@@ -29,8 +29,81 @@ void CAA_Cloud::render() {
 	m_pSprite->SetPos(pos);
 
 	//Draw Lightening
-	//if (m_fLighteningState > 3.0f) {//draw whole lightening
-	if (true) {//TODO
+	if ((0.5f < m_fLighteningState) && (m_fLighteningState <= 1.0f)) {
+		const int n = 4;
+		Sint16 vx [n];
+		Sint16 vy [n];
+		//MBE cast pos...
+		vx[0] = pos.x+70;
+		vy[0] = 45;
+
+		vx[1] = pos.x+75;
+		vy[1] = 200;
+
+		vx[2] = pos.x+100;
+		vy[2] = 170;
+
+		vx[3] = pos.x+60;
+		vy[3] = 200;
+
+
+		g_pFramework->drawTexturedPolygon(vx, vy, n, m_pTextureSprite);
+	}
+
+	if ((1.0f < m_fLighteningState) && (m_fLighteningState <= 1.5f)) {
+			const int n = 5;
+			Sint16 vx [n];
+			Sint16 vy [n];
+			//MBE cast pos...
+			vx[0] = pos.x+70;
+			vy[0] = 45;
+
+			vx[1] = pos.x+75;
+			vy[1] = 200;
+
+			vx[2] = pos.x+100;
+			vy[2] = 170;
+
+
+			vx[3] = pos.x+60;
+			vy[3] = 200;
+
+			vx[4] = pos.x+40;
+			vy[4] = 250;
+
+
+			g_pFramework->drawTexturedPolygon(vx, vy, n, m_pTextureSprite);
+		}
+
+	if ((1.5f < m_fLighteningState) && (m_fLighteningState <= 2.0f)) {
+		const int n = 6;
+		Sint16 vx [n];
+		Sint16 vy [n];
+		//MBE cast pos...
+		vx[0] = pos.x+70;
+		vy[0] = 45;
+
+		vx[1] = pos.x+75;
+		vy[1] = 200;
+
+		vx[2] = pos.x+100;
+		vy[2] = 170;
+
+
+		vx[3] = pos.x+60;
+		vy[3] = 200;
+
+		vx[4] = pos.x+40;
+		vy[4] = 250;
+
+		vx[5] = pos.x+40;
+		vy[5] = 250;
+
+
+		g_pFramework->drawTexturedPolygon(vx, vy, n, m_pTextureSprite);
+	}
+
+	if (m_fLighteningState > 2.0f) {//draw whole lightening
 		const int n = 7;
 		Sint16 vx [n];
 		Sint16 vy [n];
@@ -54,15 +127,13 @@ void CAA_Cloud::render() {
 		vx[5] = pos.x+40;
 		vy[5] = 250;
 
-		vx[6] = pos.x+50;//TODO = vx[0]
+		vx[6] = pos.x+50;// = vx[0]...
 		vy[6] = 45;
 
 		//als pointer übergeben???
 		//arrays sind also immer zeiger auf den ersten wert... lustig
-		g_pFramework->drawTexturedPolygon(vx, vy, 7, m_pTextureSprite);
+		g_pFramework->drawTexturedPolygon(vx, vy, n, m_pTextureSprite);
 	}
-
-
 
 	//render cloud over Polygon
 	m_pSprite->Render();
@@ -73,9 +144,12 @@ void CAA_Cloud::update() {
 	FloatRect r = getRect();
 	r.x = m_pAimWorm->getRect().x-50.0f;
 
-	m_fLighteningState += 1.0f * g_pTimer->getElapsed();
+	m_fLighteningState += 12.0f * g_pTimer->getElapsed();
 
 	setRect(r);
+
+
+	m_pAimWorm->changeEnergyBy( -14.0f * g_pTimer->getElapsed() );
 
 }
 
@@ -84,7 +158,7 @@ void CAA_Cloud::quit() {
 }
 
 void CAA_Cloud::init(CVec StartPos, CWorm * pAimWorm, CWorm * pOwnerWorm, int TeamID) {
-	initKillTime(5.0f);
+	initKillTime(0.7f);
 	FloatRect fr = StartPos.toFloatRect();
 	fr.w = m_pSprite->GetRect().w;
 	fr.h = m_pSprite->GetRect().h;
